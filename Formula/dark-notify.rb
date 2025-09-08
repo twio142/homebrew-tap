ae26b84c60b4049a9c60db63465ec74c135012f3
twio142 <64556708+twio142@users.noreply.github.com>
2025-09-08 23:01:58 +0200

    feat(formulae): introduce template-based formula management
    This change introduces a template-based system for managing Homebrew formulae. This is necessary to support automated version bumping with GitHub Actions, especially for complex, multi-architecture formulae that are not supported by tools like `mislav/bump-homebrew-formula-action` out of the box.
    
    - Templates are created for `ttyd` and `rime-ls` which have conditional logic for different architectures.\n- All existing formulae have been updated to use placeholder values for `version` and `sha256`, which will be populated from the templates during the release process.

--- Formula/dark-notify.rb
+++ Formula/dark-notify.rb
@@ -1,9 +1,9 @@
 class DarkNotify < Formula
   desc "Watcher for macOS 10.14+ light/dark mode changes"
   homepage "https://github.com/twio142/dark-notify"
-  version "0.1.2"
+  version "..."
+  sha256 "..."
   url "https://github.com/twio142/dark-notify/releases/download/v#{version}/dark-notify-arm64"
-  sha256 "987c4e40ca9f7996f72d8967a74417e2fc7e8d7aea02e93bd314f80a80817f9a"
 
   def install
     bin.install "dark-notify-arm64" => "dark-notify"
