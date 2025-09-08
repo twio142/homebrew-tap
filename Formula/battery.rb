b80ef060ea6cceb7dd98d87c877a9a6de721e069
twio142 <64556708+twio142@users.noreply.github.com>
2025-09-08 23:01:58 +0200

    feat(formulae): introduce template-based formula management
    This change introduces a template-based system for managing Homebrew formulae. This is necessary to support automated version bumping with GitHub Actions, especially for complex, multi-architecture formulae that are not supported by tools like `mislav/bump-homebrew-formula-action` out of the box.
    
    - Templates are created for `ttyd` and `rime-ls` which have conditional logic for different architectures.\n- All existing formulae have been updated to use placeholder values for `version` and `sha256`, which will be populated from the templates during the release process.

--- Formula/battery.rb
+++ Formula/battery.rb
@@ -1,9 +1,9 @@
 class Battery < Formula
   desc "CLI for managing battery charging"
   homepage "https://github.com/twio142/battery/"
+  version "..."
+  sha256 "..."
   url "https://github.com/twio142/battery/releases/download/v#{version}/battery"
-  version "1.2.7"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000" # placeholder
 
   def install
     bin.install "battery"
--- Formula/go-pinyin.rb
+++ Formula/go-pinyin.rb
@@ -1,9 +1,9 @@
 class GoPinyin < Formula
   desc "A Go library for converting Chinese hanzi to pinyin"
   homepage "https://github.com/twio142/go-pinyin"
+  version "..."
+  sha256 "..."
   url "https://github.com/twio142/go-pinyin/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "..." # placeholder
-  version "0.1.0" # placeholder
   license "MIT"
 
   depends_on "go" => :build
