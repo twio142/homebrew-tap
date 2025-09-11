03c1c4413590487d01c4a71ae0266ccdc83f6064
twio142 <64556708+twio142@users.noreply.github.com>
2025-09-11 07:16:32 +0200

    chore(go): formula ready for use

--- Formula/go-pinyin.rb
+++ Formula/go-pinyin.rb
@@ -1,14 +1,16 @@
 class GoPinyin < Formula
-  desc "A Go library for converting Chinese hanzi to pinyin"
+  desc "Go library for converting Chinese hanzi to pinyin"
   homepage "https://github.com/twio142/go-pinyin"
-  url "https://github.com/twio142/go-pinyin/archive/refs/tags/v....tar.gz"
-  version "..."
-  sha256 "..."
+  url "https://github.com/twio142/go-pinyin/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "c3e4c0a97875494f2375be542e05d1cd2cd875310384f9b636c5136a2454a48d"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", "go-pinyin", "./cmd/pinyin"
-    bin.install "go-pinyin"
+    system "go", "build", *std_go_args(output: bin/"go-pinyin")
+  end
+
+  test do
+    system bin/"go-pinyin", "-h"
   end
 end
