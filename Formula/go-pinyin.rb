b95895b9a70e65d003a2da4ec33b5dd86a7ebe8a
twio142 <64556708+twio142@users.noreply.github.com>
2025-09-11 08:32:22 +0200

    go-pinyin 0.1.1

--- Formula/go-pinyin.rb
+++ Formula/go-pinyin.rb
@@ -1,8 +1,8 @@
 class GoPinyin < Formula
   desc "Go library for converting Chinese hanzi to pinyin"
   homepage "https://github.com/twio142/go-pinyin"
-  url "https://github.com/twio142/go-pinyin/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "c3e4c0a97875494f2375be542e05d1cd2cd875310384f9b636c5136a2454a48d"
+  url "https://github.com/twio142/go-pinyin/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "71b1469f2a0569deed9e75c06f9c962aef4500afaf81cd8a6959f50bdb8601cc"
 
   depends_on "go" => :build
 
