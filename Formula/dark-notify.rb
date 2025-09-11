bcfbee17548524c3bb2262ba6b2949c1970f1983
twio142 <64556708+twio142@users.noreply.github.com>
2025-09-11 09:08:45 +0200

    docs: remove to-dos

--- Formula/dark-notify.rb
+++ Formula/dark-notify.rb
@@ -2,7 +2,6 @@ class DarkNotify < Formula
   desc "Watcher for macOS 10.14+ light/dark mode changes"
   homepage "https://github.com/twio142/dark-notify"
   url "https://github.com/twio142/dark-notify/releases/download/v1.0.2-mod/dark-notify-arm64.tar.gz"
-  version "1.0.2-mod"
   sha256 "b15ebbc9eccfc4bed57b7c6e7142fe29577d5405bf4c30370ea059e6fd86d9ec"
 
   depends_on arch: :arm64
@@ -13,6 +12,6 @@ class DarkNotify < Formula
   end
 
   test do
-    system "#{bin}/dark-notify", "--help"
+    system bin/"dark-notify", "--help"
   end
 end
