0f585b24758bcc7cbd0235be59d06143d76a41ea
twio142 <64556708+twio142@users.noreply.github.com>
2025-09-11 04:41:44 +0000

    chore: bump ttyd to v1.7.7-mod

--- Formula/ttyd.rb
+++ Formula/ttyd.rb
@@ -1,23 +1,23 @@
 class Ttyd < Formula
   desc "Command-line tool for sharing terminal over the web"
   homepage "https://github.com/twio142/"
-  version "..."
+  version "1.7.7-mod"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/twio142/ttyd/releases/download/v#{version}/ttyd-arm64-macos.tar.gz"
-      sha256 "..."
+      sha256 "7f00cba9ab3acb6d3bfb69b01662d8daf1dbc3b4171630dc307acad5549dd623"
     else
       url "https://github.com/twio142/ttyd/releases/download/v#{version}/ttyd-x86_64-macos.tar.gz"
-      sha256 "..."
+      sha256 "8cd1b3ef48b9dec4cbd080c4279106966900517fb522fed34b6dcd6bbd1910dc"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/twio142/ttyd/releases/download/v#{version}/ttyd-x86_64-linux.tar.gz"
-      sha256 "..."
+      sha256 "a6f99c82de6a41cb068853b2d4fa767ae2e5e839dc5fa40a2caa5e368cec1c50"
     end
   end
 
