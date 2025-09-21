6c04445e879974cc8196822716d508054d7c1f70
twio142 <64556708+twio142@users.noreply.github.com>
2025-09-21 22:49:32 +0200

    refactor!(rime-ls): use bottles for pre-built binaries
    move ci workflow from upstream to tap repo
    
    fix

--- Formula/rime-ls.rb
+++ Formula/rime-ls.rb
@@ -1,27 +1,24 @@
 class RimeLs < Formula
   desc "Language server for text input with rime"
-  homepage "https://github.com/twio142/rime-ls"
-  version "0.4.3-mod"
+  homepage "https://github.com/wlh320/rime-ls"
+  url "https://github.com/wlh320/rime-ls/archive/refs/tags/v0.4.3.tar.gz"
+  sha256 "38a13b1ba9cbf833a0d96c12a0b4cc38c8feb73d6efbd3c4002e498ab29a3d2b"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/twio142/rime-ls/releases/download/v#{version}/rime_ls-aarch64-apple-darwin.tar.gz"
-      sha256 "58e201a7be3cfaf73e384f135e8c74c25d784662019157031b17be18d71d3e1b"
-    else
-      url "https://github.com/twio142/rime-ls/releases/download/v#{version}/rime_ls-x86_64-apple-darwin.tar.gz"
-      sha256 "5f3535e1b8581c958e287e56922210f72da697ec6bbe5a45d0c6a78b929244c2"
-    end
+  livecheck do
+    url :stable
+    strategy :github_latest
   end
 
-  on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/twio142/rime-ls/releases/download/v#{version}/rime_ls-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "cc53ebc590fa0a9c12b41bdba1dd4c1767fff50d7436abff01afd8ee2e461bf1"
-    end
-  end
+  depends_on "rust" => :build
+  depends_on "librime"
 
   def install
-    bin.install Dir["rime_ls*"].first => "rime_ls"
+    # Set environment variables to point to the Homebrew-installed librime
+    ENV["LIBRIME_INCLUDE_DIR"] = Formula["librime"].opt_include
+    ENV["LIBRIME_LIB_DIR"] = Formula["librime"].opt_lib
+
+    system "cargo", "build", "--release"
+    bin.install "target/release/rime_ls"
   end
 
   test do
