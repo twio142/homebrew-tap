a7c20e773b12286c855c46099765051a2321a1eb
twio142 <64556708+twio142@users.noreply.github.com>
2025-09-23 16:31:35 +0000

    dark-notify 0.1.3

--- Formula/dark-notify.rb
+++ Formula/dark-notify.rb
@@ -1,13 +1,18 @@
 class DarkNotify < Formula
   desc "Watcher for macOS 10.14+ light/dark mode changes"
   homepage "https://github.com/cormacrelf/dark-notify"
-  url "https://github.com/cormacrelf/dark-notify/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "802aee7ea3e3dd6bf5a8a4b451b9310582fc9f04a336907affd19f97a8cda679"
+  url "https://github.com/cormacrelf/dark-notify/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "468bce03cd5e6f0d8c56253293fa3f9334fbb354dc189958a0573a9d9232dca7"
   head "https://github.com/cormacrelf/dark-notify.git", branch: "master"
 
   livecheck do
     url :stable
     strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/twio142/homebrew-tap/releases/download/dark-notify-v0.1.3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "36ecda871f3eceef8bc35a6a1cedb06be423103dc96f32220a8cf7694802e91b"
   end
 
   depends_on "rust" => :build
