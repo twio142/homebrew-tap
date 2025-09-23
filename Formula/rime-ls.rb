96f737228653b801499f89506b3b42daec1ddc42
twio142 <64556708+twio142@users.noreply.github.com>
2025-09-23 02:45:35 +0200

    refactor(rime-ls): generalize rime-ls update workflow for reuse
    Update GitHub Actions workflow to use environment variables for formula
    and
    upstream repository, replacing hardcoded references to rime-ls. This
    enables
    easier reuse and maintenance for similar Homebrew formula update
    workflows.
    All formula-related steps now reference $FORMULA and $UPSTREAM
    variables.

--- Formula/rime-ls.rb
+++ Formula/rime-ls.rb
@@ -3,6 +3,7 @@ class RimeLs < Formula
   homepage "https://github.com/wlh320/rime-ls"
   url "https://github.com/wlh320/rime-ls/archive/refs/tags/v0.4.3.tar.gz"
   sha256 "38a13b1ba9cbf833a0d96c12a0b4cc38c8feb73d6efbd3c4002e498ab29a3d2b"
+  license "BSD-3-Clause"
 
   livecheck do
     url :stable
