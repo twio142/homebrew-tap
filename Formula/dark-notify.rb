b035af35de85844d19d3c80df418b8bf2eba91f4
twio142 <64556708+twio142@users.noreply.github.com>
2025-09-23 02:48:24 +0200

    refactor(dark-notify): move ci to tap repo
    formula point to original repo instead of my fork
    build bottle

--- Formula/dark-notify.rb
+++ Formula/dark-notify.rb
@@ -1,14 +1,22 @@
 class DarkNotify < Formula
   desc "Watcher for macOS 10.14+ light/dark mode changes"
-  homepage "https://github.com/twio142/dark-notify"
-  url "https://github.com/twio142/dark-notify/releases/download/v1.0.2-mod/dark-notify-arm64.tar.gz"
+  homepage "https://github.com/cormacrelf/dark-notify"
+  url "https://github.com/cormacrelf/dark-notify/archive/refs/tags/v0.1.2.tar.gz"
   sha256 "b15ebbc9eccfc4bed57b7c6e7142fe29577d5405bf4c30370ea059e6fd86d9ec"
+  head "https://github.com/cormacrelf/dark-notify.git", branch: "master"
 
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "rust" => :build
   depends_on arch: :arm64
   depends_on :macos
 
   def install
-    bin.install Dir["dark-notify*"].first => "dark-notify"
+    system "rustup", "toolchain", "install", "nightly"
+    system "cargo", "+nightly", "install", *std_cargo_args
   end
 
   test do
