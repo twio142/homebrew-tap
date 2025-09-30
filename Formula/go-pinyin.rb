fb3a10bcbf7e49f3f7b760d3b7374d66bc0cea0a
twio142 <64556708+twio142@users.noreply.github.com>
2025-09-30 22:59:41 +0000

    go-pinyin 0.1.1 bottle block [skip ci]

--- Formula/go-pinyin.rb
+++ Formula/go-pinyin.rb
@@ -9,6 +9,13 @@ class GoPinyin < Formula
     strategy :git
   end
 
+  bottle do
+    root_url "https://github.com/twio142/homebrew-tap/releases/download/go-pinyin-v0.1.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5e28102fda073db33191bec5b7cb5acd3736570542ba9c16299f7d8fe9b4f2b0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "368da54e6cfa44e60816629ce93948181dc09e1c43489c1c5dba2683488edf40"
+  end
+
   depends_on "go" => :build
 
   def install
