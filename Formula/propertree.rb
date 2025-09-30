0f6a21f8e59b64c37ca84a8aa28fa073ae4bed51
autobump <autobump@bot>
2025-09-30 07:53:33 +0000

    Bump propertree to 25.09.28-7cd845d9

--- Formula/propertree.rb
+++ Formula/propertree.rb
@@ -1,10 +1,10 @@
 class Propertree < Formula
   desc "Cross-platform GUI plist editor written in python"
   homepage "https://github.com/corpnewt/ProperTree"
-  url "https://github.com/corpnewt/ProperTree.git#25.08.31-417837ad",
-    revision: "417837ad"
-  version "25.08.31-417837ad"
-  sha256 "958f9320b3db36987dd323db7731549cfec0e35d4bf3e5b8a2ca2e9b3c458a38"
+  url "https://github.com/corpnewt/ProperTree.git#25.09.28-7cd845d9",
+    revision: "7cd845d9"
+  version "25.09.28-7cd845d9"
+  sha256 "6dbd289927cba9ea7499e9e1363a53be8b5750aae50a2a01596353e81b48e17a"
   license "BSD-3-Clause"
 
   livecheck do
