54b137d40bf27350ff76b14f5c9bd00ec3f4c9c7
twio142 <64556708+twio142@users.noreply.github.com>
2025-10-01 00:42:24 +0000

    ttyper 1.6.0 bottle block [skip ci]

--- Formula/ttyper.rb
+++ Formula/ttyper.rb
@@ -10,6 +10,13 @@ class Ttyper < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/twio142/homebrew-tap/releases/download/ttyper-v1.6.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a9443a0ea52f360e1d351e28353fb983900df24ffff259beaac7455f4a642c24"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f2165507a4b26781f491217dd7c7df30a336150db113e3ac35def602f14acffb"
+  end
+
   depends_on "rust" => :build
 
   def install
