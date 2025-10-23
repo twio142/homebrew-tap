dbda11ce55cd83e28e0f7243451a6fb319a29ba1
twio142 <actions@github.com>
2025-10-23 15:45:52 +0000

    reddittui 0.3.9 bottle block [skip ci]

--- Formula/reddittui.rb
+++ Formula/reddittui.rb
@@ -10,6 +10,13 @@ class Reddittui < Formula
     strategy :git
   end
 
+  bottle do
+    root_url "https://github.com/twio142/homebrew-tap/releases/download/reddittui-v0.3.9"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7979771708766eef0b2ec1fc2c273aab99f54094521e0fec6f3f753c4cfa836b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "92f4b1fdd85ed0afb62f63d11ec5db3e945abe73469e8df3265de199b1d42765"
+  end
+
   depends_on "go" => :build
 
   def install
