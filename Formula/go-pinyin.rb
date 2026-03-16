fc2f2c6123d1ed67bad02cf17777beac62e464ac
twio142 <64556708+twio142@users.noreply.github.com>
2026-03-16 14:35:52 +0100

    go-pinyin 0.1.2 (#41)
    * go-pinyin 0.1.2
    
    * go-pinyin 0.1.2 bottle block [skip ci]
    
    ---------
    
    Co-authored-by: twio142 <autobump@bot>

--- Formula/go-pinyin.rb
+++ Formula/go-pinyin.rb
@@ -1,8 +1,8 @@
 class GoPinyin < Formula
   desc "Go library for converting Chinese hanzi to pinyin"
   homepage "https://github.com/twio142/go-pinyin"
-  url "https://github.com/twio142/go-pinyin/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "e7fca4a5537230f30b448feb648edee3b0eab214a9d6f0a0c977b953370ee17f"
+  url "https://github.com/twio142/go-pinyin/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "ee074a8b33a56ca93517997f62f19133e08777163f38ef0e23cf3162257ef44e"
 
   livecheck do
     url :homepage
@@ -10,10 +10,9 @@ class GoPinyin < Formula
   end
 
   bottle do
-    root_url "https://github.com/twio142/homebrew-tap/releases/download/go-pinyin-v0.1.1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5e28102fda073db33191bec5b7cb5acd3736570542ba9c16299f7d8fe9b4f2b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "368da54e6cfa44e60816629ce93948181dc09e1c43489c1c5dba2683488edf40"
+    root_url "https://github.com/twio142/homebrew-tap/releases/download/go-pinyin-v0.1.2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d72ae96487dbdda338cc73d1b383fd60ade5d7253b5809d36e54715634f89526"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "acee78ec17c0d7406176dc0de7ec4d6f7e1ffd79a2730903127c2a5babf8f7db"
   end
 
   depends_on "go" => :build
