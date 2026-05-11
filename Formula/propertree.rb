f5459de2d618fb9894673c643baa237a5b481596
twio142 <64556708+twio142@users.noreply.github.com>
2026-05-11 04:18:04 +0200

    propertree 26.05.10-a10a7f17 (#43)
    * propertree 26.05.10-a10a7f17
    
    * propertree 26.05.10-a10a7f17 bottle block [skip ci]
    
    ---------
    
    Co-authored-by: twio142 <autobump@bot>

--- Formula/propertree.rb
+++ Formula/propertree.rb
@@ -3,8 +3,8 @@ class Propertree < Formula
   homepage "https://github.com/corpnewt/ProperTree"
   url "https://github.com/corpnewt/ProperTree.git",
     using:    :git,
-    revision: "ed0c7742381201f737ecfcc508028ee58f9374f0"
-  version "25.12.25-ed0c7742"
+    revision: "a10a7f17af06e68212f70bb73390787628c2d32f"
+  version "26.05.10-a10a7f17"
   license "BSD-3-Clause"
 
   livecheck do
@@ -24,8 +24,8 @@ class Propertree < Formula
   end
 
   bottle do
-    root_url "https://github.com/twio142/homebrew-tap/releases/download/propertree-v25.12.25-ed0c7742"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e4bc0aecde62e332730db57c0cfde65b263ef3ab2ed7970f658d670ab9afd42d"
+    root_url "https://github.com/twio142/homebrew-tap/releases/download/propertree-v26.05.10-a10a7f17"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "175fa1d94d1491dc3c7790fc3539532f203327373b11521653b03494878d40ab"
   end
 
   depends_on "python-tk@3.13"
