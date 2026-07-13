898f377690e03600beb4c204fceb4332faa80a70
twio142 <64556708+twio142@users.noreply.github.com>
2026-07-13 14:23:22 +0200

    systemctl-tui 0.6.0 (#47)
    * systemctl-tui 0.6.0
    
    * fix(ci): trust the tap
    
    * systemctl-tui 0.6.0 bottle block [skip ci]
    
    ---------
    
    Co-authored-by: twio142 <autobump@bot>

--- Formula/systemctl-tui.rb
+++ Formula/systemctl-tui.rb
@@ -1,8 +1,8 @@
 class SystemctlTui < Formula
   desc "Fast, simple TUI for interacting with systemd services and their logs"
   homepage "https://github.com/rgwood/systemctl-tui"
-  url "https://github.com/rgwood/systemctl-tui/archive/refs/tags/v0.5.2.tar.gz"
-  sha256 "1c77ba06ece0866c6680eb52c2842d65ed688e273d9eb2e310106cdfa841d701"
+  url "https://github.com/rgwood/systemctl-tui/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "279daea0c3664087f45763d347544f92117b821cbeef01aef4f02e21b4f11ceb"
   license "MIT"
 
   livecheck do
@@ -11,8 +11,8 @@ class SystemctlTui < Formula
   end
 
   bottle do
-    root_url "https://github.com/twio142/homebrew-tap/releases/download/systemctl-tui-v0.5.2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "864dbf710a8854507cefe0274f3962f1f7f57749c8c2ffe64af93601d6f9444f"
+    root_url "https://github.com/twio142/homebrew-tap/releases/download/systemctl-tui-v0.6.0"
+    sha256 cellar: :any, x86_64_linux: "ef05a813b6a8bb5b794967939d4a0b2536c6d0827f1d3d04bb56147b8dc581d2"
   end
 
   depends_on "rust" => :build
