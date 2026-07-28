668a2b9b14c55fb358d2083ee700d484064add89
twio142 <64556708+twio142@users.noreply.github.com>
2026-07-28 11:57:45 +0200

    systemctl-tui 0.8.0 (#50)
    * systemctl-tui 0.8.0
    
    * fix(systemctl-tui): specify path
    
    * systemctl-tui 0.8.0 bottle block [skip ci]
    
    ---------
    
    Co-authored-by: twio142 <autobump@bot>

--- Formula/systemctl-tui.rb
+++ Formula/systemctl-tui.rb
@@ -1,8 +1,8 @@
 class SystemctlTui < Formula
   desc "Fast, simple TUI for interacting with systemd services and their logs"
   homepage "https://github.com/rgwood/systemctl-tui"
-  url "https://github.com/rgwood/systemctl-tui/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "7ce318bb5f9fa4fc4b266557d1476bd8ed058faa15807a579090bd93e994ab23"
+  url "https://github.com/rgwood/systemctl-tui/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "004e160f4f59f824ac35baf23fbc615b9f12bdb4f5eee122017bfe24a6ec02d9"
   license "MIT"
 
   livecheck do
@@ -11,8 +11,8 @@ class SystemctlTui < Formula
   end
 
   bottle do
-    root_url "https://github.com/twio142/homebrew-tap/releases/download/systemctl-tui-v0.7.0"
-    sha256 cellar: :any, x86_64_linux: "b5060955b441d6b48800be9bcbed20285214b732cd6e30cd8237e3694758373c"
+    root_url "https://github.com/twio142/homebrew-tap/releases/download/systemctl-tui-v0.8.0"
+    sha256 cellar: :any, x86_64_linux: "68c8c1e5ba6f4626d502803cfdbe2d0e2a71f8a17aed1c8545ae4bceeeee52a9"
   end
 
   depends_on "rust" => :build
